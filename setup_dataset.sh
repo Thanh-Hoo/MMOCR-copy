ac466d2b52eb76b16379e4dd5a266908a4ef7ed8
Thanh-Hoo <62130953+Thanh-Hoo@users.noreply.github.com>
2021-11-09 20:45:29 +0700

    first commit

--- setup_dataset.sh
+++ setup_dataset.sh
@@ -9,6 +9,8 @@ mv data_pan/train/img imgs/training
 mv data_pan/val/img imgs/test
 mv data_pan/train/label annotations/training
 mv data_pan/val/label annotations/test
+rm -rf data_pan
+rm -rf data_pan_14_points.zip
 cd ..
 
 python3 tools/data/textdet/ctw1500_converter.py data -o data --split-list training test
