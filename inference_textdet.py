af35cec7c9a6a60355f91efe13cc2ef6b4668ab3
ThanhHo54 <62130953+Thanh-Hoo@users.noreply.github.com>
2021-11-15 01:55:56 +0700

    img

--- inference_textdet.py
+++ inference_textdet.py
@@ -120,8 +120,7 @@ def single_gpu_test(model,
         with torch.no_grad():
             # result boundingbox
             img = Image.open("/content/MMOCR-copy/data/imgs/test/0_PVPwIDcHCoL_Jau1fSr3rwlV5W1Vl3aX7xyoK7F14-1.png")
-            # print(data)
-            result = model(return_loss=False, rescale=True, img)
+            result = model(return_loss=False, rescale=True, **img)
 
         batch_size = len(result)
         if show or out_dir:
