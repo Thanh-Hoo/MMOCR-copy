01b350fbb0706eac57aa941b76a11c87c89f5103
ThanhHo54 <62130953+Thanh-Hoo@users.noreply.github.com>
2021-12-25 03:17:32 +0700

    code predict

--- tools/predict_text_det.py
+++ tools/predict_text_det.py
@@ -9,10 +9,10 @@ def convert_xyminmax(list_box):
     new_list = []
     for box in list_box[0]["boundary_result"]:
         box = box[:-1]
-        xmin = int(min(box[0::2]))
-        xmax = int(max(box[0::2]))
-        ymin = int(min(box[1::2]))
-        ymax = int(max(box[1::2]))
+        xmin = int(min(box[0::2])-5)
+        xmax = int(max(box[0::2])+5)
+        ymin = int(min(box[1::2])-5)
+        ymax = int(max(box[1::2])+5)
         new_list.append([xmin, ymin, xmax, ymax])
     return new_list
 
