e270a9cb72ee571798a518df25292d1a09371b5a
ThanhHo54 <62130953+Thanh-Hoo@users.noreply.github.com>
2021-12-25 02:01:15 +0700

    code predict

--- mmocr/utils/predict_text_det.py
+++ mmocr/utils/predict_text_det.py
@@ -5,12 +5,20 @@ import glob
 ocr = MMOCR(det='PANet_CTW', recog=None)
 # process bbox
 
-def find_xyminmax(list_box):
-    pass
+def convert_xyminmax(list_box):
+    new_list = []
+    for box in list_box:
+        xmin = min(box[0::2])
+        xmax = max(box[0::2])
+        ymin = min(box[1::2])
+        ymax = max(box[1::2])
+        new_list.append([xmin, ymin, xmax, ymax])
+    return new_list
 
 # Inference
 count_time_1= time.time()
 for img in glob.glob("/content/MMOCR-copy/data/imgs/test/*"):
     results = ocr.readtext(img, output='hello.jpg', export='./')
+    results = convert_xyminmax(results)
     print(results)
 print(time.time() - count_time_1)
