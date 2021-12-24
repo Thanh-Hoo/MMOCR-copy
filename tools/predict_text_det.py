11c1a5b0fb98f9d91b6a4a96d764de8b45eda299
ThanhHo54 <62130953+Thanh-Hoo@users.noreply.github.com>
2021-12-25 03:12:00 +0700

    code predict

--- tools/predict_text_det.py
+++ tools/predict_text_det.py
@@ -8,11 +8,11 @@ ocr = MMOCR(det='PANet_CTW', recog=None)
 def convert_xyminmax(list_box):
     new_list = []
     for box in list_box[0]["boundary_result"]:
-        print(box)
-        xmin = min(box[0:-1:2])
-        xmax = max(box[0:-1:2])
-        ymin = min(box[1:-1:2])
-        ymax = max(box[1:-1:2])
+        box = box[:-1]
+        xmin = min(box[0::2])
+        xmax = max(box[0::2])
+        ymin = min(box[1::2])
+        ymax = max(box[1::2])
         new_list.append([xmin, ymin, xmax, ymax])
     return new_list
 
